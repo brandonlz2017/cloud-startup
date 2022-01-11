a002af50579331181b43e843c53fd41d2b6831a8
brandonlz2017 <94196603+brandonlz2017@users.noreply.github.com>
2022-01-10 23:38:57 -0500

    Create get-json.py

---
+++ get-json.py
@@ -0,0 +1,9 @@
+import requests, json
+
+url = requests.get('https://opendata.arcgis.com/datasets/3eeb0a2cbae94b3e8549a8193717a9e1_0.geojson')
+
+text = url.text
+
+data = json.loads(text)
+
+print(data)
