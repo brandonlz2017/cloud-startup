a9c0b643d3f6b6b124255bd540bd318504e4b5a0
brandonlz2017 <94196603+brandonlz2017@users.noreply.github.com>
2022-01-11 23:02:45 -0500

    Update get-json.py

--- get-json.py
+++ get-json.py
@@ -5,5 +5,15 @@ url = requests.get('https://opendata.arcgis.com/datasets/3eeb0a2cbae94b3e8549a81
 text = url.text
 
 data = json.loads(text)
+data = data['features']
 
-print(data['features'][0]['properties'])
+temp_array = []
+for row in range(len(data)):
+    row_working = data[row]['properties']
+    temp_array.append(row_working)
+
+print(temp_array[:5)    
+    
+    
+
+#print(data['features'][0]['properties'])
