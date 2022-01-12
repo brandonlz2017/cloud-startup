8e0bec3111f9d5a31332a2eea940b083a998fc3b
brandonlz2017 <94196603+brandonlz2017@users.noreply.github.com>
2022-01-12 01:07:53 -0500

    Update get-json.py

--- get-json.py
+++ get-json.py
@@ -2,6 +2,9 @@ import requests, json
 import pandas as pd
 
 url = requests.get('https://opendata.arcgis.com/datasets/3eeb0a2cbae94b3e8549a8193717a9e1_0.geojson')
+crime_codes = pd.read_csv('https://www.arcgis.com/sharing/rest/content/items/e6ca4eadecdc475a961f68bc314e2a86/data')
+crime_name_dict = dict(zip(list(crime_codes['CODE']), list(crime_codes['NAME'])))
+crime_type_dict = dict(zip(list(crime_codes['CODE']), list(crime_codes['VIO_PROP_CFS'])))
 
 text = url.text
 
@@ -17,6 +20,10 @@ for row in range(len(data)):
 df = pd.DataFrame(temp_array)
 df = df.drop([16], axis=1)
 df.columns = columns
+
+df['CrimeCodeName'] = df['CrimeCode'].map(crime_name_dict)
+df['CrimeCodeType'] = df['CrimeCode'].map(crime_type_dict)
+
 
 print(df.head())    
     
